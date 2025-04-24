e61522df4dc9951c73f2b6b20ab3d48c1d530883
Ahmed Ilyas <ahmed.n.ilyas@gmail.com>
2025-04-24 14:33:43 +0200

    Add base url

--- tests/test_docs.py
+++ tests/test_docs.py
@@ -1,9 +1,20 @@
 
 from nomad.config import config
+import pytest
+from fastapi.testclient import TestClient
+
+from nomad.app.main import app
+
+
+
+@pytest.fixture(scope="session")
+def client():
+    return TestClient(app, base_url="http://testserver/")
 
 
 def test_docs(client):
-    rv = client.get('/docs/index.html')
+    app_base = config.services.api_base_path
+    rv = client.get(f'{app_base}/docs/index.html')
     assert rv.status_code == 200
     assert (
         f'max-age={config.services.html_resource_http_max_age}, must-revalidate'
@@ -11,7 +22,7 @@ def test_docs(client):
     )
     assert 'Etag' in rv.headers
 
-    rv = client.get('/docs/assets/favicon.png')
+    rv = client.get(f'{app_base}/docs/assets/favicon.png')
     assert rv.status_code == 200
     assert (
         f'max-age={config.services.image_resource_http_max_age}, must-revalidate'
@@ -20,7 +31,7 @@ def test_docs(client):
     assert 'Etag' in rv.headers
 
     etag = rv.headers['Etag']
-    rv = client.get('/docs/assets/favicon.png', headers={'If-None-Match': etag})
+    rv = client.get(f'{app_base}/docs/assets/favicon.png', headers={'If-None-Match': etag})
     assert rv.status_code == 304
-    rv = client.get('/docs/assets/favicon.png', headers={'If-None-Match': f'W/{etag}'})
+    rv = client.get(f'{app_base}/docs/assets/favicon.png', headers={'If-None-Match': f'W/{etag}'})
     assert rv.status_code == 304
